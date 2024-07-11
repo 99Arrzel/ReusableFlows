4629a3e6e0f908af75375deca35d0dd4ac30b845
Andres Carrillo <af.carrillo@live.com>
2024-07-11 11:10:40 -0400

    Api Builder and readme

--- index.tsx
+++ index.tsx
@@ -5,6 +5,7 @@ import {
   useInfiniteQuery as useInfiniteQueryTanstack,
   UseMutationOptions,
   InfiniteData,
+  useQueryClient,
 } from "@tanstack/react-query";
 import { z } from "zod";
 export type TRequestType = "get" | "post" | "put" | "delete";
@@ -17,7 +18,7 @@ export type TBuildOutput<TInferedOutput, TOutput> =
 
 /**
  * A procedure is the start of a query/mutation, a wrapper that allows us to declare a query/mutation
- * Additionally, if the schema has an input, this is validated, so we make sure that the input is valid, both for 
+ * Additionally, if the schema has an input, this is validated, so we make sure that the input is valid, both for
  * mutations and queries
  */
 export class Procedure<TInput, TOutput = unknown, TError = unknown> {
@@ -225,4 +226,65 @@ export class Procedure<TInput, TOutput = unknown, TError = unknown> {
 export class Flow {
   static procedure = new Procedure();
 }
-export default Flow;
+export type ReturnUseUtils<ResultFlow> = {
+  [K in keyof ResultFlow]: {
+    [K2 in keyof ResultFlow[K]]: ResultFlow[K][K2] extends {
+      useQuery: any;
+      ctx: any;
+    }
+      ? {
+          invalidate: () => Promise<void>;
+          refetch: () => Promise<void>;
+        }
+      : never;
+  };
+};
+
+export class Utils<T extends Record<string, Record<string, any>>> {
+  private val: T;
+  constructor(val: T) {
+    this.val = val;
+  }
+
+  /**
+   * TODO: Fix type casting with generics or infer, idk
+   */
+  useUtils = () => {
+    const queryClient = useQueryClient();
+    return Object.entries(this.val).reduce((prev, [key, value]) => {
+      const values = Object.entries(value).reduce((prev, [key2, value2]) => {
+        if (value2.ctx.type === "mutation" || !value2.ctx.type) {
+          return {
+            ...prev,
+          };
+        }
+        return {
+          ...prev,
+          [key2]: {
+            invalidate: () => {
+              return queryClient.invalidateQueries({
+                queryKey: value2.ctx.key,
+              });
+            },
+            refetch: () => {
+              return queryClient.refetchQueries({
+                queryKey: value2.ctx.key,
+              });
+            },
+          },
+        };
+      }, {});
+      return {
+        [key]: values,
+        ...prev,
+      };
+    }, {}) as ReturnUseUtils<T>;
+  };
+}
+//TODO: Fix non any types
+export const apiBuilder = <FuncionList extends Record<string, any>>(
+  val: FuncionList
+) => {
+  return { utils: new Utils(val), ...val };
+};
+
---
+++ readme.md
@@ -0,0 +1,40 @@
+## Reusable Flows
+
+Reusable flows is a small package that helps you organize your code using Tanstack Query, it's used for declaring TS inputs and outputs, the objective is to type whatever your api is calling for (Thus aimed to non ts backends)
+
+It's influenced by TRPC so the usage is similar
+
+Just a drop in and use, but you have to configure tanstack working. Zod library is used for schema validations.
+
+
+```typescript
+//First declare an object containing procedures
+ const tickets = {
+  closeTicket: Flow.procedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+  )
+    .output(z.object({ id: z.string() }))
+    .mutation(async ({ input, ctx}) => {
+      const { data } = await LocalRequestHelper({
+        url: URLHelper("rrhh.tickets/closeTicket"),
+        requestType: "post",
+        data: input,
+      })
+      return data as z.infer<typeof ctx.schemeOutput>
+    }, `cerrarTicket-tickets`),
+}
+//Then add it as a route which is an object with more objects
+const routes = {
+  other,
+  tickets,
+}
+//And then build the api with apiBuilder
+export const api = apiBuilder(routes)
+
+
+```
+
+That's all, others should be able to use your defined routes, with your specific inputs and outputs fully typed!
